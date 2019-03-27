cc9a4b295340552ccbcc02dfe4f119fb04f40444
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-27 20:32:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ function distanceTravelledInFeet(start, destination) {
   }
 }
 function calculatesFarePrice(start, destination) {
-  if(distanceTravelledInFeet(start, destination) < 2000)
+  if(400 < distanceTravelledInFeet(start, destination) < 2000)
 }
